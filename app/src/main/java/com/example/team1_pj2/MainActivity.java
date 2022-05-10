84ae3c997e457cd1837add9e74068c2056f57aa1
1871344park <gwonnn@naver.com>
2022-05-10 18:27:14 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/team1_pj2/MainActivity.java
+++ app/src/main/java/com/example/team1_pj2/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.team1_pj2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new Month_View_Fragment());
+        fragmentTransaction.commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // 메인 메뉴랑 연결
+        MenuInflater inflater  = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { // 메뉴 클릭 이벤트 처리
+        switch (item.getItemId()) {
+            case R.id.action_month_view: // 월달력 이벤트
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new Month_View_Fragment());
+                fragmentTransaction.commit();
+                return true;
+
+            case R.id.action_week_view: // 주달력 이벤트
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, new WeekViewFragment());
+                fragmentTransaction.commit();
+                return true;
+
+            default:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
